5466d5a673b9de492d2dc5b0e370d5ef8321af1f
Lauro Jacques <lauro.jacques@dtidigital.com.br>
2018-04-29 23:34:09 -0300

    monstro reclamando do menu

--- js/scripts-comuns.js
+++ js/scripts-comuns.js
@@ -1,4 +1,7 @@
 document.querySelector("#menu-toggle").addEventListener('click', (event => {
   let menu = document.querySelector("#menu-itens");
   menu.classList.toggle('aberto');
+
+  let monstro = document.querySelector("#monster");
+  monstro.classList.toggle('eating');
 }));
