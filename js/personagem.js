ef4467679ca0c244e36877ee0d6d05a5fb6e36b0
root <daniel0547@gmail.com>
2018-04-30 02:23:07 +0000

    pulo+gravidade

--- js/personagem.js
+++ js/personagem.js
@@ -1,16 +1,37 @@
-var limiteEsquerdo = 0;
-var limiteDireito = 100;
-var posicao = limiteEsquerdo+1;
+let limiteEsquerdo = 0;
+let limiteBaixo = 0;
+let limiteDireito = 10000;
+let posicao = limiteEsquerdo+1;
+let posicaoy = limiteBaixo+1;
+let monstro = document.querySelector("#monster");
+window.setInterval(gravidade, 100);
+
+function gravidade(){
+	if(posicaoy < 0){
+		posicaoy += 10;
+		monstro.style.marginTop = posicaoy + "px";
+	}
+}
+
 document.addEventListener('keydown', (event => {
-  let monstro = document.querySelector("#monster");
   debugger;
   if (event.keyCode == 37 && posicao > limiteEsquerdo) {
-    posicao -= 1;
+    posicao -= 5;
     monstro.style.marginLeft = posicao + "px";
   }
   if (event.keyCode == 39 && posicao < limiteDireito) {
-    posicao += 1;
+    posicao += 5;
     monstro.style.marginLeft = posicao + "px";
   }
+  if (event.keyCode == 38 && posicao < limiteDireito) {
+	if(posicaoy >=0){
+		posicaoy -= 50;
+		monstro.style.marginTop = posicaoy + "px";
+	}
+  }
+  //if (event.keyCode == 40 && posicao < limiteDireito) {
+  //  posicaoy += 50;
+  //  monstro.style.marginTop = posicaoy + "px";
+  //}
 }));
 
