59fd1c10414ac3ba78e3441c456715bb176af95e
Lauro Jacques <lauro.jacques@dtidigital.com.br>
2018-05-03 20:36:03 -0300

    Widget de trocar personagem funcionando ok

--- js/personagem.js
+++ js/personagem.js
@@ -1,81 +1,85 @@
 let limiteEsquerdo = 0;
 let limiteBaixo = 0;
 let limiteDireito = 10000;
-let posicao = limiteEsquerdo+1;
-let posicaoy = limiteBaixo+1;
+let posicao = limiteEsquerdo + 1;
+let posicaoy = limiteBaixo + 1;
 let monstro = document.querySelector("#monster");
 window.setInterval(gravidade, 100);
-let type="pato";
+let type = "pato";
+
+let botaoTrocarPersonagem = document.querySelector("#trocarPersonagem");
+botaoTrocarPersonagem.addEventListener('click', mudaPersonagem);
 
 
-function gravidade(){
-	if(posicaoy < 0){
+function gravidade() {
+	if (posicaoy < 0) {
 		posicaoy += 10;
 		monstro.style.marginTop = posicaoy + "px";
-		if(posicaoy>=0)
-			monstro.classList.remove(type+'-pulo');
-			monstro.classList.add(type+'-parado');
+		if (posicaoy >= 0)
+			monstro.classList.remove(type + '-pulo');
+		monstro.classList.add(type + '-parado');
 	}
 }
 
 document.addEventListener('keydown', (event => {
-  if (event.keyCode == 37 && posicao > limiteEsquerdo) {
-    posicao -= 5;
-    monstro.style.marginLeft = posicao + "px";
-	monstro.classList.remove(type+'-parado');
-	monstro.classList.remove(type+'-direita');
-	monstro.classList.add(type+'-esquerda');
-  }
-  if (event.keyCode == 39 && posicao < limiteDireito) {
-    posicao += 5;
-    monstro.style.marginLeft = posicao + "px";
-	monstro.classList.remove(type+'-parado');
-	monstro.classList.remove(type+'-esquerda');
-	monstro.classList.add(type+'-direita');
-  }
-  if (event.keyCode == 38 && posicao < limiteDireito) {
-	monstro.classList.remove(type+'-esquerda');
-	monstro.classList.remove(type+'-direita');
-	monstro.classList.remove(type+'-parado');
-	if(posicaoy >=0){
-		monstro.classList.add(type+'-pulo');
-		posicaoy -= 50;
-		monstro.style.marginTop = posicaoy + "px";
+	if (event.keyCode == 37 && posicao > limiteEsquerdo) {
+		posicao -= 5;
+		monstro.style.marginLeft = posicao + "px";
+		monstro.classList.remove(type + '-parado');
+		monstro.classList.remove(type + '-direita');
+		monstro.classList.add(type + '-esquerda');
 	}
-  }
-  if (event.keyCode == 40) {
-	monstro.classList="";
-	
-	if(type==="pato"){
-		type="coelho";
-		monstro.classList.add(type+'-parado');
-		return;
+	if (event.keyCode == 39 && posicao < limiteDireito) {
+		posicao += 5;
+		monstro.style.marginLeft = posicao + "px";
+		monstro.classList.remove(type + '-parado');
+		monstro.classList.remove(type + '-esquerda');
+		monstro.classList.add(type + '-direita');
 	}
-	if(type==="coelho"){
-		type="pato";
-		monstro.classList.add(type+'-parado');
-		return;
+	if (event.keyCode == 38 && posicao < limiteDireito) {
+		monstro.classList.remove(type + '-esquerda');
+		monstro.classList.remove(type + '-direita');
+		monstro.classList.remove(type + '-parado');
+		if (posicaoy >= 0) {
+			monstro.classList.add(type + '-pulo');
+			posicaoy -= 50;
+			monstro.style.marginTop = posicaoy + "px";
+		}
 	}
-  }
-  //if (event.keyCode == 40 && posicao < limiteDireito) {
-  //  posicaoy += 50;
-  //  monstro.style.marginTop = posicaoy + "px";
-  //}
+	//if (event.keyCode == 40 && posicao < limiteDireito) {
+	//  posicaoy += 50;
+	//  monstro.style.marginTop = posicaoy + "px";
+	//}
 }));
 
 document.addEventListener('keyup', (event => {
-  if (event.keyCode == 37 && posicao > limiteEsquerdo) {
-    monstro.classList.remove(type+'-esquerda');
-	monstro.classList.remove(type+'-direita');
-	monstro.classList.add(type+'-parado');
-  }
-  if (event.keyCode == 39 && posicao < limiteDireito) {
-    monstro.classList.remove(type+'-esquerda');
-	monstro.classList.remove(type+'-direita');
-	monstro.classList.add(type+'-parado');
-  }
-  if (event.keyCode == 39 && posicao < limiteDireito) {
-    monstro.classList.remove(type+'-pulo');
-	monstro.classList.add(type+'-parado');
-  }
+	if (event.keyCode == 37 && posicao > limiteEsquerdo) {
+		monstro.classList.remove(type + '-esquerda');
+		monstro.classList.remove(type + '-direita');
+		monstro.classList.add(type + '-parado');
+	}
+	if (event.keyCode == 39 && posicao < limiteDireito) {
+		monstro.classList.remove(type + '-esquerda');
+		monstro.classList.remove(type + '-direita');
+		monstro.classList.add(type + '-parado');
+	}
+	if (event.keyCode == 39 && posicao < limiteDireito) {
+		monstro.classList.remove(type + '-pulo');
+		monstro.classList.add(type + '-parado');
+	}
 }));
+
+function mudaPersonagem() {
+	monstro.classList = "";
+
+	if (type === "pato") {
+		type = "coelho";
+		monstro.classList.add(type + '-parado');
+		return;
+	}
+	if (type === "coelho") {
+		type = "pato";
+		monstro.classList.add(type + '-parado');
+		return;
+	}
+}
