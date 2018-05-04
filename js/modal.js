12314398c73077513481aac83a607dd20bd98567
Lauro Jacques <lauro.jacques@dtidigital.com.br>
2018-05-03 23:08:14 -0300

    tudo louco nada funciona batata frita

--- js/modal.js
+++ js/modal.js
@@ -98,7 +98,7 @@ const colocarMusica = () => {
     '</form>';
   $('#menu-itens').html(menu);
   debugger;
-  $(".target").change(function(e) {
+  $(".target").change(function() {
     e;
     debugger;
     criaAudio('https://ia800801.us.archive.org/11/items/TheImperialMarch/The-Imperial-March.mp3');
