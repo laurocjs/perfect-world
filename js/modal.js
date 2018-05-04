b125da8cc6fc043b73689d8a3de55b2672ed8a36
Lauro Jacques <lauro.jacques@dtidigital.com.br>
2018-05-03 23:14:21 -0300

    musica blur

--- js/modal.js
+++ js/modal.js
@@ -51,83 +51,83 @@ botaoAvancar.addEventListener('click', () => trocaImagem(1));
 
 // Novos itens menu
 
-const menuInicialBackup = document.querySelector('#menu-itens');
+// const menuInicialBackup = document.querySelector('#menu-itens');
 
-const setIntro = text => {
-  localStorage.setItem('emphatic', text);
-  $('.emphatic').html(text);
-}
+// const setIntro = text => {
+//   localStorage.setItem('emphatic', text);
+//   $('.emphatic').html(text);
+// }
 
-const menuInicial = () => {
-  $('#menu-itens').html(menuInicialBackup);
-}
+// const menuInicial = () => {
+//   $('#menu-itens').html(menuInicialBackup);
+// }
 
-const trocaDetalhes = () => {
-  let menu = '<form>' +
-    '<article class="form-item">' +
-    '<input name="nome" class="target" type="text" placeholder="Titulo">' +
-    '</article>' +
-    '<article class="form-item">' +
-    '<input name="nome" type="text" placeholder="Favicon">' +
-    '</article>' +
-    '</form>';
-  $('#menu-itens').html(menu);
-  $(".target").change(function () {
-    alert("Handler for .change() called.");
-  });
+// const trocaDetalhes = () => {
+//   let menu = '<form>' +
+//     '<article class="form-item">' +
+//     '<input name="nome" class="target" type="text" placeholder="Titulo">' +
+//     '</article>' +
+//     '<article class="form-item">' +
+//     '<input name="nome" type="text" placeholder="Favicon">' +
+//     '</article>' +
+//     '</form>';
+//   $('#menu-itens').html(menu);
+//   $(".target").change(function () {
+//     alert("Handler for .change() called.");
+//   });
 
-}
+// }
 
-const criarGaleria = () => {
-  let menu = '<h1>Criar Galeria</h1><article data-item="0">voltar</article>'
-  $('#menu-itens').html(menu);
-}
+// const criarGaleria = () => {
+//   let menu = '<h1>Criar Galeria</h1><article data-item="0">voltar</article>'
+//   $('#menu-itens').html(menu);
+// }
 
-const trocarPersonagem = () => {
-  let menu = '<h1>Troca Personagem</h1><article data-item="0">voltar</article>'
-  $('#menu-itens').html(menu);
-}
+// const trocarPersonagem = () => {
+//   let menu = '<h1>Troca Personagem</h1><article data-item="0">voltar</article>'
+//   $('#menu-itens').html(menu);
+// }
 
-const colocarMusica = () => {
-  let menu = '<form>' +
-    '<article data-item="0"> &lt;' +
-    '</article>' +
-    '<article class="form-item">' +
-    '<input name="nome" class="target" type="text" placeholder="Url da música">' +
-    '</article>' +
-    '</form>';
-  $('#menu-itens').html(menu);
-  debugger;
-  $(".target").change(function() {
-    e;
-    debugger;
-    criaAudio('https://ia800801.us.archive.org/11/items/TheImperialMarch/The-Imperial-March.mp3');
-  });
-  $('#menu-itens').html(menu);
-}
+// const colocarMusica = () => {
+//   let menu = '<form>' +
+//     '<article data-item="0"> &lt;' +
+//     '</article>' +
+//     '<article class="form-item">' +
+//     '<input name="nome" class="target" type="text" placeholder="Url da música">' +
+//     '</article>' +
+//     '</form>';
+//   $('#menu-itens').html(menu);
+//   debugger;
+//   $(".target").change(function() {
+//     e;
+//     debugger;
+//     criaAudio('https://ia800801.us.archive.org/11/items/TheImperialMarch/The-Imperial-March.mp3');
+//   });
+//   $('#menu-itens').html(menu);
+// }
 
-const alterarChao = () => {
-  let menu = '<h1>Alterar Galeria</h1><article data-item="0">voltar</article>'
-  $('#menu-itens').html(menu);
-}
+// const alterarChao = () => {
+//   let menu = '<h1>Alterar Galeria</h1><article data-item="0">voltar</article>'
+//   $('#menu-itens').html(menu);
+// }
 
-$("#menu-itens").on('click', 'article',
-  (res) => {
-    let item = $(res.target).data('item');
-    let text = 'Item ' + item;
-    if (item == 0)
-      menuInicial();
-    else if (item == 1)
-      trocaDetalhes();
-    else if (item == 2)
-      trocaFavicon();
-    else if (item == 3)
-      criarGaleria();
-    else if (item == 4)
-      trocarPersonagem();
-    else if (item == 5)
-      colocarMusica();
-    else if (item == 6)
-      alterarChao();
-  }
-);
+// $("#menu-itens").on('click', 'article',
+//   (res) => {
+//     let item = $(res.target).data('item');
+//     let text = 'Item ' + item;
+//     if (item == 0)
+//       menuInicial();
+//     else if (item == 1)
+//       trocaDetalhes();
+//     else if (item == 2)
+//       trocaFavicon();
+//     else if (item == 3)
+//       criarGaleria();
+//     else if (item == 4)
+//       trocarPersonagem();
+//     else if (item == 5)
+//       colocarMusica();
+//     else if (item == 6)
+//       alterarChao();
+//   }
+// );
