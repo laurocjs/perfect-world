44885f8b3bcc68f87c0cc7b9f3c27a848c8e98d3
Lucas Ramon <lucasramonsa@gmail.com>
2018-06-22 22:16:06 -0300

    upload de arquivos

---
+++ client/js/upload.js
@@ -0,0 +1,47 @@
+$('.upload-btn').on('click', function (){
+    $('#upload-input').click();
+    $('.progress-bar').text('0%');
+    $('.progress-bar').width('0%');
+    console.log('teste');
+});
+
+$('#upload-input').on('change', function(){
+
+  console.log('teste');
+
+  var files = $(this).get(0).files;
+
+  if (files.length > 0){
+    var formData = new FormData();
+    for (var i = 0; i < files.length; i++) {
+      var file = files[i];
+      formData.append('uploads[]', file, file.name);
+    }
+
+    $.ajax({
+      url: '/upload',
+      type: 'POST',
+      data: formData,
+      processData: false,
+      contentType: false,
+      success: function(data){
+          console.log('upload successful!\n' + data);
+      },
+      progress: function() {
+        var progress = new XMLHttpRequest();
+        progress.upload.addEventListener('progress', function(evt) {
+          if (evt.lengthComputable) {
+            var percentComplete = evt.loaded / evt.total;
+            percentComplete = parseInt(percentComplete * 100);
+            $('.progress-bar').text(percentComplete + '%');
+            $('.progress-bar').width(percentComplete + '%');
+            if (percentComplete === 100) {
+              $('.progress-bar').html('Done');
+            }
+          }
+        }, false);
+        return progress;
+      }
+    });
+  }
+});
--- server/app.js
+++ server/app.js
@@ -1,11 +1,52 @@
 
 var express = require('express'),
 	fs = require("fs"),
-    app = express();
-
+  app = express(),
+	path = require('path'),
+	formidable = require('formidable');
 
 app.use(express.static('client'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(3000, function () {
   console.log('Listening on port 3000');
 });
+
+app.get('/', function(req, res){
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.post('/upload', function(req, res){
+
+  console.log(req);
+
+  // create an incoming form object
+  var form = new formidable.IncomingForm();
+
+  // specify that we want to allow the user to upload multiple files in a single request
+  form.multiples = true;
+
+  // store all uploads in the /uploads directory
+  form.uploadDir = path.join(__dirname, '/uploads');
+
+  // every time a file has been uploaded successfully,
+  // rename it to it's orignal name
+  form.on('file', function(field, file) {
+    fs.rename(file.path, path.join(form.uploadDir, file.name));
+  });
+
+  // log any errors that occur
+  form.on('error', function(err) {
+    console.log('An error has occured: \n' + err);
+  });
+
+  // once all the files have been uploaded, send a response to the client
+  form.on('end', function() {
+    res.end('success');
+  });
+
+  // parse the incoming request containing the form data
+  form.parse(req);
+
+});
+
